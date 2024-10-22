914ba993d2caeb6a8517eb4253d4d702a2196823
chaitanya malik <chaitanya.malik@sabpaisa.in>
2024-10-22 18:12:31 +0530

    work commit

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Suspense } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App/>
+    <Suspense>
+      <App />
+    </Suspense>
   </BrowserRouter>
-    
 );
 
 // If you want to start measuring performance in your app, pass a function
