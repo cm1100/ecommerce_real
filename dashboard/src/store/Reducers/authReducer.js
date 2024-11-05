7a490115f7c36ddcf8fd125e340b94ea8bac18bd
chaitanya malik <chaitanya.malik@sabpaisa.in>
2024-11-05 17:56:56 +0530

    latest push

--- dashboard/src/store/Reducers/authReducer.js
+++ dashboard/src/store/Reducers/authReducer.js
@@ -3,15 +3,19 @@ import api from "../../api/api";
 
 export const admin_login  = createAsyncThunk(
     'auth/admin_login',
-    async(info)=>{
+    async(info,{rejectWithValue,fulfillWithValue})=>{
         console.log(info)
         try{
-            // const {data} = await api.post('/admin-login',info,{withCredentials:true});
-            // console.log(data)
+            const {data} = await api.post('/admin-login',info,{withCredentials:true});
+            
+            localStorage.setItem('accessToken',data.token)
+
+            return fulfillWithValue(data)
 
         }
-        catch(err){
-
+        catch(error){
+            console.log(error.response.data)
+            return rejectWithValue(error.response.data)
         }
     }
 )
@@ -25,8 +29,27 @@ export const authReducer = createSlice({
         loader:false,
         userInfo:''
     },
-    reducers:{},
-    extraReducers:()=>{}
+    reducers:{
+        messageClear:(state,_)=>{
+            state.errorMessage=''
+            state.successMessage=''
+        }
+
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(admin_login.pending,(state,{payload})=>{
+            state.loader= true;
+        }).addCase(admin_login.rejected,(state,{payload})=>{
+            state.loader=false;
+            state.errorMessage=payload.error
+        }).addCase(
+            admin_login.fulfilled,(state,{payload})=>{
+                state.loader=false;
+                state.successMessage=payload.message
+            }
+        )
+    }
 });
 
+export const {messageClear}=authReducer.actions
 export default authReducer.reducer
